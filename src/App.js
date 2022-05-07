59056215c50b0cad1f7056903eb146703494452c
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-05-07 00:18:43 -0500

    agregado el navbar y los iconos de fontawaseno

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "normalize.css";
+import React from "react";
 import NavBar from "./components/NavBar";
+import Organization from "./components/Organization";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
+      <NavBar />
+      <Organization />
+      <SearchBar></SearchBar>
     </div>
   );
 }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./navbar.module.css";
+import { FaBell, FaQuestionCircle, FaToolbox } from "react-icons/fa";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div>Mandu</div>
+        <div>Dashboard</div>
+        <div>Organization</div>
+        <div>Modelos</div>
+        <div>Seguimiento</div>
+      </div>
+      <div className={styles.box}>
+        <FaToolbox size={25} color="white" />
+        <FaQuestionCircle size={25} color="white" />
+        <FaBell size={25} color="white" />
+        <div> image</div>
+        <div>Adminsitrador</div>
+        <div> Mandu</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
