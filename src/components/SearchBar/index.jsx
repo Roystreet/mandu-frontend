07fa6ac56be83533e1a2e8a8b1712c2faccf3670
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-05-08 02:44:26 -0500

    agregado el select de busqueda, falta solo agregar las apis

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styles from "./searchbar.module.css";
+import { Select, Input } from "antd";
+const { Option } = Select;
+const { Search } = Input;
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container}>
+        <div
+          style={{
+            border: "1px solid  #378FEE",
+            color: "#378FEE",
+            background: "#EEF3FA",
+          }}
+        >
+          Listado
+        </div>
+        <div>Arbol</div>
+      </div>
+      <div>
+        <Select placeholder="Columnas" style={{ width: 120 }}>
+          <Option value="Division">Division</Option>
+          <Option value="Division Superior">Division Superior</Option>
+          <Option value="Colaboradores">Colaboradores</Option>
+          <Option value="Nivel">Nivel</Option>
+          <Option value="Subdivisiones">Subdivisiones</Option>
+          <Option value="Embajadores">Embajadores</Option>
+        </Select>
+        <Search style={{ width: 200 }}></Search>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
